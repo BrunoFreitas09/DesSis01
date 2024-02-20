6f8553090712a131827c08bb300a4d9d250db6fa
Usuario <Usuario@CPD03-M04>
2024-02-20 12:27:55 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("primeeira aula de lógica");
+            Console.WriteLine(2.5);
+            Console.WriteLine(2);
+            Console.WriteLine(2 + 2);
+            Console.WriteLine("O resultado é: " + ( 2 + 2));
+            Console.WriteLine(2 + 2 + " é o resultado ");
+            Console.WriteLine((2 + 2) * 3);
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+
+            Console.ReadKey();
+        }
+    }
+}
